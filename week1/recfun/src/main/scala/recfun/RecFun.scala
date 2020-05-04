e8bfdc1a7514e165e960f94a3b0c59baccdbfc7f
Jose Chu <jchudb93@gmail.com>
2020-05-04 11:51:25 -0500

    example

--- week1/recfun/src/main/scala/recfun/RecFun.scala
+++ week1/recfun/src/main/scala/recfun/RecFun.scala
@@ -14,33 +14,15 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
-    if(c==0 || r==c) 1
-    else pascal(c - 1, r - 1) + pascal(c, r - 1);
-  }
+  def pascal(c: Int, r: Int): Int = ???
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = {
-    def balanced(chars: List[Char], numOpens: Int): Boolean = {
-      if(chars.isEmpty) numOpens==0
-      else if (chars.head == '(') balanced(chars.tail, numOpens + 1)
-      else if (chars.head == ')') numOpens > 0 && balanced(chars.tail, numOpens - 1)
-      else balanced(chars.tail,   numOpens)
-    }
-
-    balanced(chars,0)
-  }
+  def balance(chars: List[Char]): Boolean = ???
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-    if(money==0) 1
-    else if(money > 0 && !coins.isEmpty)
-      countChange(money - coins.head, coins) + countChange(money, coins.tail)
-    else
-      0
-  }
+  def countChange(money: Int, coins: List[Int]): Int = ???
 }
